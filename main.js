281383e8db523a4645d5810739242e61a92d7bf3
Makar Kamardin <vsyakofazno@gmail.com>
2026-04-24 10:51:56 +0300

    first condition

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const grades = [
+    { name: "Макар", score: 85 },
+    { name: "Денис", score: 92 },
+    { name: "Анна", score: 78 },
+    { name: "Даша", score: 88 },
+    { name: "Студент_X", score: 45 }
+];
