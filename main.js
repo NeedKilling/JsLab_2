b93c0e3799cf5b2febc206dece075c66145084e6
Нияз <niaz@MacBook-Air-Niaz.local>
2026-04-24 23:33:29 +0300

    feat: добавил функцию...

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ const grades = [
     { name: "Даша", score: 88 },
     { name: "Студент_X", score: 45 }
 ];
+function filterfail(data, score=60 ){
+    let fail=[];
+
+    for (let i =0; i < data.length; i++){
+        if (data[i].score < score){
+            fail.push(data[i].name);
+        }
+    }
+    return fail;
+}
+console.log("Список должников", filterfail(grades));
